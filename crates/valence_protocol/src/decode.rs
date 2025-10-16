67f7496b73079972cc31e4d50af161295a06043d
Ben Heidemann <ben@heidemann.dev>
2025-10-16 19:09:09 +0100

    feat: add PacketDecoder::take_buffer

--- crates/valence_protocol/src/decode.rs
+++ crates/valence_protocol/src/decode.rs
@@ -29,6 +29,11 @@ impl PacketDecoder {
         Self::default()
     }
 
+    // Consumes the packet decoder returning all bytes ingested
+    pub fn take_buffer(self) -> BytesMut {
+        self.buf
+    }
+
     pub fn try_next_packet(&mut self) -> anyhow::Result<Option<PacketFrame>> {
         let mut r = &self.buf[..];
 
